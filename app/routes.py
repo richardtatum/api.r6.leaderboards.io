8a6b7ec10e9138f1d1934f9e863e26a7693d0c42
richardtatum <DP@DP.com>
2019-06-13 10:23:59 +0100

    Removed unused imports and added basic auth.

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,29 @@
-from app import app, db, api
+from app import api
 from flask_restful import Resource, reqparse, abort
 from app.models import User, Data, Diff, Challenge
+import os
 
 parser = reqparse.RequestParser()
 parser.add_argument('username', type=str, help='Only accepts valid site user IDs.')
+parser.add_argument('authorization', type=str)
 parser.add_argument('challenges', help='Currently Weekly Challenges')
 
-# u = User.query.get(21)
 
 def abort_user_search(user_id):
     if not User.query.filter_by(r6_user=user_id).first():
         abort(404, message=f'User with name {user_id} does not exist')
 
 
+def auth_required(auth):
+    if auth != os.environ['API_AUTH']:
+        abort(401, message='Unauthorized.')
+
+
 class UserSearch(Resource):
     def get(self):
         args = parser.parse_args()
         abort_user_search(args['username'])
+        auth_required(args['authorization'])
         u = User.query.filter_by(r6_user=args['username']).first()
         d = Data.query.filter_by(author=u).first()
         w = Diff.query.filter_by(author=u).first()
@@ -46,6 +53,8 @@ class UserSearch(Resource):
 
 class Challenges(Resource):
     def get(self):
+        args = parser.parse_args()
+        auth_required(args['authorization'])
         output = {}
         for c in Challenge.query.all():
             output[c.name] = c.details
